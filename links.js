9a2085bed8c5c20065522c4115f2d7170bc317f3
Tiago <tsomda@gmail.com>
2025-04-19 10:36:38 -0500

    better file naming

---
+++ links.js
@@ -0,0 +1,5 @@
+// Links data
+const links = [
+    { name: 'drisommer.com', url: 'https://www.drisommer.com' },
+    { name: 'tiago.dev', url: 'https://www.tiago.dev' },
+];
--- somda-network.js
+++
@@ -1,5 +0,0 @@
-// Sommer Damasceno Network data
-const somdaNetwork = [
-    { name: 'tiago.dev', url: 'https://www.tiago.dev' },
-    { name: 'drisommer.com', url: 'https://www.drisommer.com' },
-];
