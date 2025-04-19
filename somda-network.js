96eadb116042ce02ff8e0f54caa866d9611cf426
Tiago <tsomda@gmail.com>
2025-04-19 10:17:49 -0500

    network hub feature added

---
+++ somda-network.js
@@ -0,0 +1,5 @@
+// Sommer Damasceno Network data
+const somdaNetwork = [
+    { name: 'tiago.dev', url: 'https://www.tiago.dev' },
+    { name: 'drisommer.com', url: 'https://www.drisommer.com' },
+];
